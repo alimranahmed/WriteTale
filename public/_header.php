12e061c6cd1de47ba89b5cad0e57d53b67bab8ac
Al- Imran Ahmed <al.imran.cse@gmail.com>
2020-06-19 11:37:51 +0600

    WIP: navbar design

--- public/_header.php
+++ public/_header.php
@@ -9,7 +9,7 @@
   <title>Write Tale</title>
 </head>
 <body class="bg-gray-100">
-<div class="border-b">
+<div class="border-b border-blue-300">
 
   <div class="mb-0 py-2 flex items-center justify-between mx-6 sm:mx-20 md:mx-32 lg:mx-64
           px-1 sm:px-10 md:px-16 lg:px-20 xl:px-32">
@@ -18,9 +18,11 @@
       <img src="build/img/profile.png" alt="logo" class="rounded-full h-10 w-10 object-cover object-top">
     </a>
 
-    <div class="">
+    <div>
+      <input class="rounded-full border border-blue-300 outline-none px-2 focus:shadow w-full">
+    </div>
+    <div>
       <a href="#" class="uppercase hover:underline">Articles</a>
-      <a href="#" class="uppercase hover:underline ml-2">Projects</a>
       <a href="#" class="uppercase hover:underline ml-2">About</a>
     </div>
 
